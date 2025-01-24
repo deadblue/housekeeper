cf7af8f65ab69a03f0e6fa7c8dff02f3b5eb67a2
Tomo Kunagisa <root@dead.blue>
2025-01-24 23:50:07 +0800

    Rename Housekeeper to Manager.

--- manager.go
+++ manager.go
@@ -9,7 +9,7 @@ const (
 	_MethodInit = "Init"
 )
 
-type Housekeeper struct {
+type Manager struct {
 	cache map[string]reflect.Value
 }
 
@@ -22,62 +22,62 @@ Exmaple:
 
 	type Foo struct {}
 
-	hk := New()
+	mgr := New()
 	var foo *Foo
-	if err := hk.Get(&foo); err != nil {
+	if err := mgr.Get(&foo); err != nil {
 		log.Fatal(err)
 	}
 	// TODO: Works with foo
 */
-func (h *Housekeeper) Get(value any) (err error) {
+func (m *Manager) Get(value any) (err error) {
 	rv := reflect.ValueOf(value)
 	rt := rv.Type()
 	if err = checkTypeForGet(rt); err != nil {
 		return
 	}
-	if v, err := h.getValue(rt.Elem()); err == nil {
+	if v, err := m.getValue(rt.Elem()); err == nil {
 		rv.Set(v.Addr())
 	}
 	return
 }
 
 // Put puts value to Housekeeper cache.
-func (h *Housekeeper) Put(value any) (err error) {
+func (m *Manager) Put(value any) (err error) {
 	rt := reflect.TypeOf(value)
 	if err = checkType(rt); err != nil {
 		return
 	}
 	cacheKey := getCacheKey(rt.Elem())
-	h.cache[cacheKey] = reflect.ValueOf(value)
+	m.cache[cacheKey] = reflect.ValueOf(value)
 	return
 }
 
 // getValue gets value from cache.
 //
 // The input t should be a pointer type.
-func (h *Housekeeper) getValue(t reflect.Type) (v reflect.Value, err error) {
+func (m *Manager) getValue(t reflect.Type) (v reflect.Value, err error) {
 	if err = checkType(t); err != nil {
 		return
 	}
 	cacheKey := getCacheKey(t)
 	var found bool
-	if v, found = h.cache[cacheKey]; !found {
-		if v, err = h.newValue(t); err == nil {
-			h.cache[cacheKey] = v
+	if v, found = m.cache[cacheKey]; !found {
+		if v, err = m.newValue(t); err == nil {
+			m.cache[cacheKey] = v
 		}
 	}
 	return
 }
 
-func (h *Housekeeper) newValue(t reflect.Type) (v reflect.Value, err error) {
+func (m *Manager) newValue(t reflect.Type) (v reflect.Value, err error) {
 	v = reflect.New(t.Elem())
-	if err = h.initValue(t, v); err != nil {
+	if err = m.initValue(t, v); err != nil {
 		v.SetZero()
 	}
 	return
 }
 
-func (h *Housekeeper) initValue(t reflect.Type, v reflect.Value) (err error) {
+func (m *Manager) initValue(t reflect.Type, v reflect.Value) (err error) {
 	// Find init method
 	im, ok := t.MethodByName(_MethodInit)
 	if !ok {
@@ -90,7 +90,7 @@ func (h *Housekeeper) initValue(t reflect.Type, v reflect.Value) (err error) {
 	args[0] = v
 	for i := 1; i < argCount; i++ {
 		// TODO: Handle circular-reference
-		args[i], err = h.getValue(ft.In(i))
+		args[i], err = m.getValue(ft.In(i))
 		if err != nil {
 			return
 		}
@@ -99,29 +99,29 @@ func (h *Housekeeper) initValue(t reflect.Type, v reflect.Value) (err error) {
 	return findError(im.Func.Call(args))
 }
 
-func (h *Housekeeper) Close() (err error) {
-	for _, rv := range h.cache {
+func (m *Manager) Close() (err error) {
+	for _, rv := range m.cache {
 		v := rv.Interface()
 		if c, ok := v.(io.Closer); ok {
 			c.Close()
 		}
 	}
-	clear(h.cache)
+	clear(m.cache)
 	return
 }
 
 // GetFrom gets value from housekeeper with generic support.
-func GetFrom[V any](hk *Housekeeper) (value *V, err error) {
+func GetFrom[V any](m *Manager) (value *V, err error) {
 	rt := reflect.TypeFor[*V]()
-	if v, err := hk.getValue(rt); err == nil {
+	if v, err := m.getValue(rt); err == nil {
 		value = v.Interface().(*V)
 	}
 	return
 }
 
 // New creates a housekeeper for you.
-func New() *Housekeeper {
-	return &Housekeeper{
+func New() *Manager {
+	return &Manager{
 		cache: make(map[string]reflect.Value),
 	}
 }
